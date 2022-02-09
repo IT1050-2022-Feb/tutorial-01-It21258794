8df6f51851fba539e25c14fdc551aaacfe2be103
kaveesha <99312274+It21258794@users.noreply.github.com>
2022-02-09 06:18:50 +0000

    completed 2

--- Tute02.c
+++ Tute02.c
@@ -20,7 +20,23 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() 
 {
-  float distance, amount
+  float distance, amount;
+
+  printf("Enter the distance :");  \\ get the distance from the user
+  scanf("%f", &distance);
+
+  if(distance <=30)
+  {
+    amount= distance * 50;    \\ canculate the amount of first 30km
+
+  }
+
+  else
+  {
+    amount= 30*50 +(distance-30)*40;  \\calculate amount of extra km
+  }
+
+  printf("Total amount :%.2f", amount); \\ display the total amount 
   
   return 0;
 }
